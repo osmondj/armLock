abeb0bb5e51e1129d08b34ec4ac4119bd7b0099d
patryk <osmaczkopatryk@gmail.com>
2017-10-28 14:01:16 +0200

    winapi example with lock

--- main.cpp
+++ main.cpp
@@ -5,9 +5,77 @@
  *      Author: User
  */
 
-#include <iostream>
+#include <windows.h>
 
-int main() {
-  std::cout << "DUPA MARYNA" << std::endl;
+LPSTR NazwaKlasy = (char*)"Klasa Okienka";
+MSG Komunikat;
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow) {
+  // WYPE£NIANIE STRUKTURY
+  WNDCLASSEX wc;
+
+  wc.cbSize = sizeof(WNDCLASSEX);
+  wc.style = 0;
+  wc.lpfnWndProc = WndProc;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hInstance = hInstance;
+  wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wc.lpszMenuName = NULL;
+  wc.lpszClassName = NazwaKlasy;
+  wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+  // REJESTROWANIE KLASY OKNA
+  if (!RegisterClassEx(&wc)) {
+    MessageBox(NULL, "Wysoka Komisja odmawia rejestracji tego okna!",
+               "Niestety...", MB_ICONEXCLAMATION | MB_OK);
+    return 1;
+  }
+
+  // TWORZENIE OKNA
+  HWND hwnd;
+
+  hwnd = CreateWindowEx(WS_EX_CLIENTEDGE, NazwaKlasy, "Oto okienko",
+                        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 240,
+                        120, NULL, NULL, hInstance, NULL);
+
+  if (hwnd == NULL) {
+    MessageBox(NULL, "Okno odmówi³o przyjœcia na œwiat!", "Ale kicha...",
+               MB_ICONEXCLAMATION);
+    return 1;
+  }
+
+  ShowWindow(hwnd, nCmdShow);  // Poka¿ okienko...
+  UpdateWindow(hwnd);
+
+  // Pêtla komunikatów
+  while (GetMessage(&Komunikat, NULL, 0, 0)) {
+    TranslateMessage(&Komunikat);
+    DispatchMessage(&Komunikat);
+  }
+  return Komunikat.wParam;
+}
+
+// OBS£UGA ZDARZEÑ
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+  switch (msg) {
+    case WM_CLOSE:
+      LockWorkStation();
+      DestroyWindow(hwnd);
+      break;
+
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
+
+    default:
+      return DefWindowProc(hwnd, msg, wParam, lParam);
+  }
+
   return 0;
 }
